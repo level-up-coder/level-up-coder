ed555810e4d86f7f86d3a0d3d9bcfb88ddc53a01
Saket Jain <saket.jain@gmail.com>
2025-12-25 10:57:04 +0530

    done

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => {
+let clicks = 0;
+let timeLeft = 5;
+let timerRunning = false;
 
-    let clicks = 0;
-    let timeLeft = 5;
-    let timer = null;
-    let timerRunning = false;
+document.getElementById("clickBtn").onclick = () => {
 
-    const clickBtn = document.getElementById("clickBtn");
-    const restartBtn = document.getElementById("restartBtn");
-    const timerText = document.getElementById("timer");
-    const cpsText = document.getElementById("cps");
-
-    clickBtn.onclick = () => {
-        if (!timerRunning) {
-            timerRunning = true;
-            startCountdown();
-        }
-
-        if (timeLeft > 0) {
-            clicks++;
-        }
-    };
-
-    function startCountdown() {
-        timerText.innerText = "Time Left: " + timeLeft;
-
-        timer = setInterval(() => {
-            timeLeft--;
-            timerText.innerText = "Time Left: " + timeLeft;
-
-            if (timeLeft <= 0) {
-                clearInterval(timer);
-                timerRunning = false;
-
-                const cps = (clicks / 5).toFixed(2);
-                cpsText.innerText = `Time up! Total Clicks: ${clicks} | CPS: ${cps}`;
-
-                clickBtn.disabled = true;
-            }
-        }, 1000);
+    if (!timerRunning) {
+        timerRunning = true;
+        startCountdown();
     }
 
-    restartBtn.onclick = () => {
-        alert("restart button"); // THIS WILL FIRE NOW
+    if (timeLeft > 0) {
+        clicks++;
+    }
+};
 
-        clearInterval(timer);
+function startCountdown() {
+    let timer = setInterval(() => {
+        timeLeft--;
 
-        clicks = 0;
-        timeLeft = 5;
-        timerRunning = false;
-        timer = null;
+        document.getElementById("timer").innerText = "Time Left: " + timeLeft;
 
-        timerText.innerText = "Time Left: 5";
-        cpsText.innerText = "CPS: 0";
-        clickBtn.disabled = false;
-    };
+        if (timeLeft === 0) {
+            clearInterval(timer);
 
-});
+            let cps = (clicks / 5).toFixed(2);
+
+            document.getElementById("cps").innerText =
+                "Time up! Total Clicks: " + clicks + " | CPS: " + cps;
+
+            document.getElementById("clickBtn").disabled = true;
+        }
+
+    }, 1000);
+}
+
+document.getElementById("restartBtn").onclick = () =>{
+    clearInterval(timer);
+
+    clicks = 0;
+    timeLeft = 5;
+    timerRunning = false;
+
+    timerText.innerText = "Time Left: 5";
+    cpsText.innerText = "CPS: 0";
+
+    clickBtn.disabled = false;
+
+};
